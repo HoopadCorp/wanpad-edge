8f7806581e6916552e80d72ab974d713a9952054
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2022-08-01 17:36:39 +0430

    Update ztp-init.sh
    removed comments

--- installation/ztp-init.sh
+++ installation/ztp-init.sh
@@ -26,17 +26,5 @@ validate_token
 change_env_file
 run_pnp_client_py
 
-#
-#check_NA $*
-#
-#if [ $skip_dialogue == true ]
-#then
-#	echo $URI $TOKEN
-#else
-#	ztp_dialogue
-#	echo $URI $TOKEN
-#fi
-#
-
 
 
