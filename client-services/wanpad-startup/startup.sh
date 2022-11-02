3e8ff5209638c7c16aa7d5428bcc7db10d78a30c
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2022-11-02 15:09:41 +0330

    Create startup.sh
    Signed-off-by: Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>

---
+++ client-services/wanpad-startup/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# You need to mention the scripts you want to 
+# run at startup here.
+
+. /etc/wanpad/client-services/wanpad-startup-1.sh
+. /etc/wanpad/client-services/wanpad-startup-2.sh
+. /etc/wanpad/client-services/wanpad-startup-3.sh
+
+
