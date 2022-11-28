733bf2aa7a6c74999e8e02ffe4ddf47aa293a662
ali ranjbar <63869905+mrAlirj@users.noreply.github.com>
2022-11-28 12:01:12 +0330

    Update pnp-client.py
    Signed-off-by: ali ranjbar <63869905+mrAlirj@users.noreply.github.com>

--- client-services/pnp-client/pnp-client.py
+++ client-services/pnp-client/pnp-client.py
@@ -61,8 +61,6 @@ def client_program():
 
             filebeat = response.get('filebeat')
             filebeat_data['output.elasticsearch']['hosts'] = filebeat.get('hosts')
-            filebeat_data['output.elasticsearch']['username'] = filebeat.get('username')
-            filebeat_data['output.elasticsearch']['password'] = filebeat.get('password')
             filebeat_data['output.elasticsearch']['ssl.certificate_authorities'] = filebeat.get('ssl_crt')
             filebeat_data['output.elasticsearch.api_key'] = f"{filebeat.get('id')}:{filebeat.get('api_key')}"
 
