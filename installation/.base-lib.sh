4cc496f1a2281cc2569fa51433b9672a6374a168
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2023-02-22 15:17:12 +0330

    Update .base-lib.sh
    Signed-off-by: Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>

--- installation/.base-lib.sh
+++ installation/.base-lib.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+
 
 function force_run_as_root () {
 
   uid=`id -u`
   if [[ $uid != 0 ]]
   then
-  echo "Please login as user \"root\" and try again.
+  echo -e "${RED}Please login as user \"root\" and try again.
   You can do this by running: 
   \"sudo -i\""
   exit 
@@ -20,7 +22,7 @@ function force_root_home_dir () {
   then
   echo ""
   else
-  echo "you need to clone the repo under \"/root\"
+  echo -e "${RED}you need to clone the repo under \"/root\"
   You can do this by running:
   \"cd /root/\"
   \"git clone https://github.com/HoopadCorp/wanpad-edge.git\"
