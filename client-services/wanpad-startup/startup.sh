cea23d7dcae81ff3960406d520f38c8775956294
Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>
2023-02-28 13:32:22 +0330

    Update startup.sh
    Signed-off-by: Mohsen K Amini <77579794+mohsenkamini@users.noreply.github.com>

--- client-services/wanpad-startup/startup.sh
+++ client-services/wanpad-startup/startup.sh
@@ -3,8 +3,10 @@
 # You need to mention the scripts you want to 
 # run at startup here.
 
-. /etc/wanpad/client-services/wanpad-startup-1.sh
-. /etc/wanpad/client-services/wanpad-startup-2.sh
-. /etc/wanpad/client-services/wanpad-startup-3.sh
+DIR="/etc/wanpad/wanpad-startup"
+
+. ${DIR}/wanpad-startup-1.sh
+. ${DIR}/wanpad-startup-2.sh
+. ${DIR}/wanpad-startup-3.sh
 
 
